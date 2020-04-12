7e31c0449b905809dabcbd3f2ec7f7a422aa5696
Jonathan Marquez <jonathanmarquezj@gmail.com>
2020-04-12 15:10:07 +0200

    Creado la pagina protencia y corregidos algunos errores de la pagina principal.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import math
 app = Flask(__name__)
 
 #                EJEMPLO
@@ -6,9 +7,24 @@ app = Flask(__name__)
 # def inicio():
 #     return render_template("<html a renderizar para darselo al usuario>")
 
+# La pagina principal
 @app.route('/')
 def inicio():
     return render_template("index.html")
+
+# Pagina potencia
+@app.route('/potencia/<int:base>/<int:exponente>',methods=["GET","POST"])
+def saluda(base,exponente):
+	if exponente >= 1:
+		resultado=base*exponente
+	if exponente == 0:
+		resultado=1
+	if exponente < 0:
+		resultado=1/base * math.abs(exponente)
+	return render_template("potencia.html",base=base,exponente=exponente,resultado=resultado)
+
+
+
 
 app.run(
 	"0.0.0.0", # Direccion ip, si es 0.0.0.0 es cualquier ip, si es 127,0,0,1 solo puede entrar desde el mismo equipo.
